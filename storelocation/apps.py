d71fb39bcf699f77a468684f85b259ef2df8ecc5
Hakob <hkeshis911@student.glendale.edu>
2023-02-13 13:01:43 -0800

    Update apps.py

--- storelocation/apps.py
+++ storelocation/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class StorelocationConfig(AppConfig):
+class StoreLocationConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'StoreLocation'
+    name = 'storelocation'
