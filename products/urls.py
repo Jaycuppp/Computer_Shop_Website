fd32ad9f4153afa533dfe2f27ce399f41fd65974
Hakob <hkeshis911@student.glendale.edu>
2023-03-06 00:58:32 -0800

    Delete products directory

--- products/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.ProductIndex),
-    path('new', views.NewProduct),
-    path('clearence', views.ClearenceProducts),
-    path('return', views.ProductReturns)
-]
