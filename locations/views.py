3e295eacc08c326caefa1f8928209225446fbf82
Hakob <hkeshis911@student.glendale.edu>
2023-03-06 00:58:23 -0800

    Delete locations directory

--- locations/views.py
+++
@@ -1,8 +0,0 @@
-from django.http import HttpResponse, HttpRequest
-from django.shortcuts import render
-from .models import Store
-
-
-def LocationIndex(request):
-    store = Store.objects.all()
-    return render(request, 'LocationIndex.html', {'Store': store})
